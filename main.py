ea9b817fb5e31445482bc445c8f1f80a0137702d
Abdelmalek Aissaoui <74867799+Abdelmalek-tchi@users.noreply.github.com>
2025-06-05 21:28:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import time
 import datetime
 from telegram import Bot
-from telegram.constants import ParseMode
 from telegram.error import TelegramError
+
 
 SOURCE_CHAT_ID = "https://t.me/blombardsignal"
 TARGET_CHAT_ID = "https://t.me/cryptsight"
@@ -14,17 +14,19 @@ def forward_recent_messages():
     thirty_minutes_ago = now - 30 * 60
 
     updates = bot.get_updates()
-    for update in updates:
-        if update.message and update.message.chat.id == SOURCE_CHAT_ID:
-            message = update.message
-            message_time = int(message.date.timestamp())
 
-            if message_time >= thirty_minutes_ago:
-                try:
-                    bot.copy_message(chat_id=TARGET_CHAT_ID, from_chat_id=SOURCE_CHAT_ID, message_id=message.message_id)
-                    print(f"✅ Copied message {message.message_id}")
-                except TelegramError as e:
-                    print(f"⚠️ Error copying message: {e}")
+    for update in updates:
+        message = update.message
+        if not message or message.chat.id != SOURCE_CHAT_ID:
+            continue
+
+        message_time = int(message.date.timestamp())
+        if message_time >= thirty_minutes_ago:
+            try:
+                bot.copy_message(chat_id=TARGET_CHAT_ID, from_chat_id=SOURCE_CHAT_ID, message_id=message.message_id)
+                print(f"✅ Copied message {message.message_id}")
+            except TelegramError as e:
+                print(f"⚠️ Error copying message: {e}")
 
 if __name__ == "__main__":
     forward_recent_messages()
